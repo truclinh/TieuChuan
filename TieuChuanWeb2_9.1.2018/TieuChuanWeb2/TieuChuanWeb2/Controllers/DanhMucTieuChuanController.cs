c15ede783d5387e040eeac5ef88064495790a9e2
Linh Thai <thaitruclinh151296@gmail.com>
2018-01-27 11:28:55 +0700

    up code lan 1-- 27/1/2018 11.28AM

--- TieuChuanWeb2_9.1.2018/TieuChuanWeb2/TieuChuanWeb2/Controllers/DanhMucTieuChuanController.cs
+++ TieuChuanWeb2_9.1.2018/TieuChuanWeb2/TieuChuanWeb2/Controllers/DanhMucTieuChuanController.cs
@@ -20,15 +20,6 @@ namespace TieuChuanWeb2.Controllers
             //}
             //return RedirectToAction("DangNhap", "TaiKhoan");
         }
-        public ActionResult ChinhSua(System.Guid id)
-        {
-            //if (Session["TaiKhoan"] != null)
-            //{
-           // var da = db.dm_tieuchuan.SingleOrDefault(n => n.id == id);
-            return View();
-            //}
-            //return RedirectToAction("DangNhap", "TaiKhoan");
-        }
         [ValidateInput(false)]
         public ActionResult DanhMucTieuChuanPartial()
         {
@@ -121,7 +112,48 @@ namespace TieuChuanWeb2.Controllers
             //return RedirectToAction("DangNhap", "TaiKhoan");
 
         }
-
+        //View Thêm mới
+        public ActionResult ThemMoi()
+        {
+            //if (Session["TaiKhoan"] != null)
+            //{
+            // var da = db.dm_tieuchuan.SingleOrDefault(n => n.id == id);
+            return View();
+            //}
+            //return RedirectToAction("DangNhap", "TaiKhoan");
+        }
+        //View Chỉnh sửa
+        public ActionResult ChinhSua(System.Guid id)
+        {
+            //if (Session["TaiKhoan"] != null)
+            //{
+            // var da = db.dm_tieuchuan.SingleOrDefault(n => n.id == id);
+            return View();
+            //}
+            //return RedirectToAction("DangNhap", "TaiKhoan");
+        }
+        //View Chỉnh sửa
+        public ActionResult Xoa(System.Guid id)
+        {
+            var model = db.dm_tieuchuan;
+            if (id != null)
+            {
+                try
+                {
+                    var item = model.FirstOrDefault(it => it.id == id);
+                    if (item != null)
+                        model.Remove(item);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            //return PartialView("_DanhMucTieuChuanPartial", model.OrderBy(n => n.ma_tieuchuan).ToList());
+            return RedirectToAction("Index","DanhMucTieuChuan");
+        }
+        //-------------------------------------- Hiện lên nội dung trong Word
         public ActionResult NoiDungPartial(System.Guid id)
         {
             var model = db.dm_tieuchuan;
@@ -133,5 +165,21 @@ namespace TieuChuanWeb2.Controllers
             x.noidungbyte = nd;
             return PartialView("_NoiDungPartial", x);
         }
+        public ActionResult NoiDungThemMoiPartial()
+        {
+            var model = db.dm_tieuchuan;
+            //var x = model.SingleOrDefault(n => n.id == new Guid("D4EF2CE0-72DE-49CD-8BC7-158CBB8CEB3F"));
+            //var x = model.SingleOrDefault(n => n.id == (System.Guid)id);
+            dm_tieuchuan x = new dm_tieuchuan();
+            x.id = Guid.NewGuid();
+            x.noidung = "";
+            // byte[] docBytes = RichEditExtension.SaveCopy("RichEditName", DevExpress.XtraRichEdit.DocumentFormat.Rtf);
+            byte[] nd = Encoding.UTF8.GetBytes(x.noidung);
+            //x.noidung = Encoding.ASCII.GetBytes(x.noidung);
+            x.noidungbyte = nd;
+            return PartialView("_NoiDungPartial", x);
+        }
+        //--------------------------------------End Hiện lên nội dung trong Word
+
     }
 }
